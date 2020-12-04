d0f3414fcde2dd7400b591853ce40e469eb34334
liujia <liujia@3golden.com.cn>
2020-12-04 18:32:43 +0800

    'charts'

--- src/views/charts/echarts.vue
+++ src/views/charts/echarts.vue
@@ -1,17 +1,28 @@
 <template>
   <a-row>
     <a-col :span="8">
-      <div></div>
+      <div id="charts" style="width: 100%;height: 200px;"></div>
     </a-col>
   </a-row>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import {Component} from 'vue-property-decorator'
+import {Vue, Component} from 'vue-property-decorator'
+import Echarts from 'echarts'
 
 @Component
 export default class Charts extends Vue{
+  public options:any={
+    series: {
+      name: 'circle',
+      type: 'bar',
 
+    }
+  }
+
+  mounted(){
+    // const charts = echarts.init(document.getElementById('charts'))
+    console.log(Echarts)
+  }
 }
 </script>
